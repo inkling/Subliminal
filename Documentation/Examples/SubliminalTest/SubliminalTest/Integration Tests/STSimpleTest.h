1a66d0bd101a003befa6028aa7ac1902813575b6
Jeff Wear <jeff@inkling.com>
2012-10-17 13:10:53 -0700

    Added a really simple test to the SubliminalTest example project.

---
+++ Documentation/Examples/SubliminalTest/SubliminalTest/Integration Tests/STSimpleTest.h
@@ -0,0 +1,13 @@
+//
+//  STSimpleTest.h
+//  SubliminalTest
+//
+//  Created by Jeffrey Wear on 10/17/12.
+//  Copyright (c) 2012 Inkling. All rights reserved.
+//
+
+#import <Subliminal/Subliminal.h>
+
+@interface STSimpleTest : SLTest
+
+@end
---
+++ Documentation/Examples/SubliminalTest/SubliminalTest/Integration Tests/STSimpleTest.m
@@ -0,0 +1,33 @@
+//
+//  STSimpleTest.m
+//  SubliminalTest
+//
+//  Created by Jeffrey Wear on 10/17/12.
+//  Copyright (c) 2012 Inkling. All rights reserved.
+//
+
+#import "STSimpleTest.h"
+
+@implementation STSimpleTest {
+    SLTextField *_usernameField;
+}
+
+- (void)setUpTestWithSelector:(SEL)testSelector {
+    _usernameField = [SLTextField elementWithAccessibilityLabel:@"username field"];
+}
+
+- (void)testThatWeCanEnterSomeText {
+    NSString *username = @"Jeff";
+    // this shouldn't throw an exception
+    [UIAElement(_usernameField) setText:username];
+    // and this should be true
+    SLAssertTrue([[UIAElement(_usernameField) text] isEqualToString:username], @"Username was not set to %@", username);
+}
+
+- (void)tearDownTestWithSelector:(SEL)testSelector {
+    if (testSelector == @selector(testThatWeCanEnterSomeText)) {
+        [self.testController sendAction:@selector(resetLogin)];
+    }
+}
+
+@end
