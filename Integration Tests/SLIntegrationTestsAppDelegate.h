84380f74cfb5ed74804405b0d912fe1e272e152d
Jeff Wear <jeff@inkling.com>
2013-01-31 14:45:08 -0800

    Added Integration Tests target.
    To test Subliminal itself.

---
+++ Integration Tests/SLIntegrationTestsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  Integration Tests
+//
+//  Created by Jeffrey Wear on 1/31/13.
+//  Copyright (c) 2013 Inkling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLIntegrationTestsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Integration Tests/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Integration Tests
+//
+//  Created by Jeffrey Wear on 1/31/13.
+//  Copyright (c) 2013 Inkling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SLIntegrationTestsAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SLIntegrationTestsAppDelegate class]));
+    }
+}
