ce26dff61b147022c0026fdc2c83b52d009728f7
Jeff Wear <jeff@inkling.com>
2013-02-18 12:04:48 -0800

    Added a file template for SLIntegrationTests.
    Installed by invoking ./Documentation/File\ Templates/InstallFileTemplates.sh with the "-d" option.

--- Documentation/File Templates/InstallFileTemplates.sh
+++ Documentation/File Templates/InstallFileTemplates.sh
@@ -1,5 +1,21 @@
 #! /bin/sh
 
+PROG_NAME=$(basename $0)
+COPY_SLINTEGRATIONTEST_TEMPLATE=false;
+
+while getopts d OPTION
+do
+    case ${OPTION} in
+        d) COPY_SLINTEGRATIONTEST_TEMPLATE=true;;
+      [?]) echo >&2 "Usage: ./${PROG_NAME} [ -d ]"
+           exit 2;;
+    esac
+done
+
 TEMPLATE_DIR="${HOME}/Library/Developer/Xcode/Templates/File Templates/Subliminal"
 mkdir -p "${TEMPLATE_DIR}"
-cp -r ./Subliminal/ "${TEMPLATE_DIR}"
+
+cp -r "./Subliminal/Integration test class.xctemplate" "${TEMPLATE_DIR}"
+if $COPY_SLINTEGRATIONTEST_TEMPLATE; then
+	cp -r "./Subliminal/Subliminal Integration test class.xctemplate" "${TEMPLATE_DIR}"
+fi
---
+++ Documentation/File Templates/Subliminal/Subliminal integration test class.xctemplate/Default/___FILEBASENAME___ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "SLTestCaseViewController.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ViewController : SLTestCaseViewController
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___ViewController
+
+/**
+ For every test case, return a non-nil value from EITHER nibNameForTestCase: 
+ OR viewForTestCase:.
+ */
+
++ (NSString *)nibNameForTestCase:(SEL)testCase {
+#warning Potentially incomplete method implementation.
+    // Return the name of the nib file which contains the view
+    // to be exercised by testCase.
+    return nil;
+}
+
++ (UIView *)viewForTestCase:(SEL)testCase {
+#warning Potentially incomplete method implementation.
+    // Return the view to be exercised by testCase.
+    return nil;
+}
+
+- (instancetype)initWithTestCaseWithSelector:(SEL)testCase {
+    self = [super initWithTestCaseWithSelector:testCase];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+@end
