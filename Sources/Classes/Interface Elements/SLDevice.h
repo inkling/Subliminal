54164d9043463dc79df3073ee4b1f0f1a26146d6
Jeff Wear <jeff@inkling.com>
2013-05-23 13:41:50 -0700

    Document SLDevice.

--- Sources/Classes/Interface Elements/SLDevice.h
+++ Sources/Classes/Interface Elements/SLDevice.h
@@ -9,18 +9,30 @@
 #import <Foundation/Foundation.h>
 
 
+/**
+ The singleton `SLDevice` instance allows you to access and manipulate
+ the device on which your application is running.
+ */
 @interface SLDevice : NSObject
 
+/**
+ Returns an object representing the current device.
+ 
+ @return A singleton object that represents the current device.
+ */
 + (SLDevice *)currentDevice;
 
 /**
- Renders the app inactive for the specified duration.
-
+ Deactivates your application for the specified duration.
+ 
+ By pushing the Home button, waiting for the specified duration, 
+ and then using the app switcher to reactivate the application.
+ 
  This method will not return until the app reactivates.
 
  Note that the time spent inactive will actually be a few seconds longer than specified.
- UIAutomation reactivates the app using the app switcher, but lingers thereupon 
- for several seconds before actually tapping the app in the switcher.
+ UIAutomation lingers upon the app switcher for several seconds before actually 
+ tapping the app.
  
  @param duration The time, in seconds, for the app to remain inactive 
  (subject to the caveat in the discussion).
