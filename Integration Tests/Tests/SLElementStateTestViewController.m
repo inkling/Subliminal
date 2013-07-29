57ee2298808f31ee0040814347444cac7eb9064f
Jeff Wear <jeff@inkling.com>
2013-07-29 15:07:34 -0700

    Remove scroll view tappability test cases from `SLElementStateTest`.
    Those cases appear to be duplicates of those in `SLElementTapTest` and that material
    is more appropriately tested in `SLElementTapTest`.

--- Integration Tests/Tests/SLElementStateTestViewController.m
+++ Integration Tests/Tests/SLElementStateTestViewController.m
@@ -30,9 +30,6 @@
 @property (weak, nonatomic) IBOutlet UIButton *button;
 @property (weak, nonatomic) IBOutlet UIView *coveringView;
 
-@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-@property (weak, nonatomic) IBOutlet UIButton *scrollViewButton;
-
 @end
 
 
@@ -48,9 +45,6 @@
                testCase == @selector(testElementIsTappableIfItHasANonNullHitpoint) ||
                testCase == @selector(testCanRetrieveLabelEvenIfNotTappable)) {
         nibName = @"SLElementStateTestCompletelyCovered";
-    } else if (testCase == @selector(testScrollViewsAreNotTappableOnIPad5_x) ||
-               testCase == @selector(testScrollViewChildElementsAreTappableEvenOnIPad5_x)) {
-        nibName = @"SLElementStateTestScrollViewCases";
     }
     return nibName;
 }
@@ -95,12 +89,6 @@
 
     _testView.isAccessibilityElement = YES;
     _testView.accessibilityLabel = @"Test Element";
-
-    // we can't make the scroll view accessible in `testScrollViewsAreNotTappableOnIPad5_x`
-    // because that will prevent its child element from appearing in the accessibility hierarchy
-    if (self.testCase == @selector(testScrollViewsAreNotTappableOnIPad5_x)) {
-        self.scrollView.accessibilityIdentifier = @"scroll view";
-    }
 }
 
 - (instancetype)initWithTestCaseWithSelector:(SEL)testCase {
