8351fd561a753e02fa35d634e3e0742fe06af473
Jeff Wear <jeff@inkling.com>
2014-01-14 00:41:12 -0800

    Slightly increase the delay before alert handlers return to ensure that alert delegates receive their callbacks.
    On certain platforms i.e. Travis, iOS 7 occasionally requires a slightly higher delay.

--- Sources/Classes/UIAutomation/User Interface Elements/SLAlert.m
+++ Sources/Classes/UIAutomation/User Interface Elements/SLAlert.m
@@ -36,7 +36,7 @@ const NSTimeInterval SLAlertHandlerDidHandleAlertDelay = 2.0;
  so that their alert's delegate receives its callbacks before the tests
  continue, assuming that the tests are waiting-with-timeout on `didHandleAlert`. 
  */
-static const NSTimeInterval SLAlertHandlerManualDelay = 0.5;
+static const NSTimeInterval SLAlertHandlerManualDelay = 0.75;
 
 
 #pragma mark - SLAlertHandler
