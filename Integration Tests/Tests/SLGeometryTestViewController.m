3bda935cf4f55b9df0e16d48a2efad4272bd98df
Jeff Wear <jeff@inkling.com>
2014-05-24 15:29:46 -0700

    Factor the construction of generic test case views into a reusable category method.
    The popover and terminal tests also use generic views but cannot leverage the
    category method because they require a reference to the label that's part of the
    generic view. Nevertheless, their layout of the label has been updated to fix a
    clipping issue discovered in the development of the category method.

--- Integration Tests/Tests/SLGeometryTestViewController.m
+++ Integration Tests/Tests/SLGeometryTestViewController.m
@@ -36,24 +36,9 @@
 
 - (void)loadViewForTestCase:(SEL)testCase
 {
-    UIView *view = [[UIView alloc] initWithFrame:self.navigationController.view.bounds];
-    view.backgroundColor = [UIColor whiteColor];
-    
-    UIFont *nothingToShowHereFont = [UIFont systemFontOfSize:18.0f];
-    NSString *nothingToShowHereText = @"Nothing to show here.";
-    CGSize nothingToShowHereSize = [nothingToShowHereText sizeWithFont:nothingToShowHereFont
-                                                     constrainedToSize:CGSizeMake(3 * CGRectGetWidth(view.bounds) / 4.0f, CGFLOAT_MAX)];
-    UILabel *nothingToShowHereLabel = [[UILabel alloc] initWithFrame:(CGRect){CGPointZero, nothingToShowHereSize}];
-    nothingToShowHereLabel.backgroundColor = view.backgroundColor;
-    nothingToShowHereLabel.font = nothingToShowHereFont;
-    nothingToShowHereLabel.numberOfLines = 0;
-    nothingToShowHereLabel.text = nothingToShowHereText;
-    nothingToShowHereLabel.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin;
-    
-    [view addSubview:nothingToShowHereLabel];
-    nothingToShowHereLabel.center = CGPointMake(CGRectGetMidX(view.bounds), CGRectGetMidY(view.bounds));
-    
-    self.view = view;
+    // Since we're just testing the geometry functions,
+    // we don't require any particular view.
+    [self loadGenericView];
 }
 
 - (void)dealloc
