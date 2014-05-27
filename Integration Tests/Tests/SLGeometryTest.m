a9997bfaf40aca9ce88d6739f5b21929aa569cba
Jeff Wear <jeff@inkling.com>
2014-05-26 19:07:16 -0700

    Add functions for comparing rects and checking rect containment to `SLGeometry.h`.
    And add a missing test for `SLUIARectFromCGRect`.

--- Integration Tests/Tests/SLGeometryTest.m
+++ Integration Tests/Tests/SLGeometryTest.m
@@ -6,10 +6,10 @@
 //  Copyright (c) 2013 Inkling. All rights reserved.
 //
 
-#import <Subliminal/Subliminal.h>
 #import "SLIntegrationTest.h"
-#import "SLGeometry.h"
-#import "SLTerminal.h"
+
+#import <Subliminal/SLGeometry.h>
+#import <Subliminal/SLTerminal+ConvenienceFunctions.h>
 
 @interface SLGeometryTest : SLIntegrationTest
 
@@ -21,12 +21,56 @@
     return @"SLGeometryTestViewController";
 }
 
-- (void)testSLCGRectFromUIARectConvertsCorrectly
-{
-    const CGRect UIARect = SLCGRectFromUIARect(@"UIATarget.localTarget().frontMostApp().navigationBar().rect()");
-    const CGRect UIKitRect = [SLAskApp(navigationBarFrameValue) CGRectValue];
+- (void)testSLUIARectFromCGRect {
+    NSString *const expectedRect = @"{ origin: { x: 5.0, y: 10.0 }, size: { width: 50.0, height: 100.0 } }";
+    NSString *const actualRect = SLUIARectFromCGRect(CGRectMake(5.0, 10.0, 50.0, 100.0));
+
+    SLAssertTrue([[[SLTerminal sharedTerminal] evalFunctionWithName:SLUIARectEqualToRectFunctionName()
+                                                           withArgs:(@[ actualRect, expectedRect ])] boolValue],
+                 @"`SLUIARectFromCGRect` did not return the expected value.");
+}
+
+- (void)testSLCGRectFromUIARect {
+    const CGRect expectedRect = CGRectMake(5.0, 10.0, 50.0, 100.0);
+    const CGRect actualRect = SLCGRectFromUIARect(@"{ origin: { x: 5.0, y: 10.0 }, size: { width: 50.0, height: 100.0 } }");
     
-    SLAssertTrue(CGRectEqualToRect(UIARect, UIKitRect), @"The frame of the main window should be the same when coming from UIAutomation or UIKit");
+    SLAssertTrue(CGRectEqualToRect(actualRect, expectedRect),
+                 @"`SLCGRectFromUIARect` did not return the expected value.");
+}
+
+- (void)testSLUIARectEqualToRect {
+    NSString *const rect1 = @"{ origin: { x: 5.0, y: 10.0 }, size: { width: 50.0, height: 100.0 } }";
+    NSString *const rect2 = @"{ origin: { x: 5.0, y: 10.0 }, size: { width: 50.0, height: 115.0 } }";
+
+    SLAssertTrue([[[SLTerminal sharedTerminal] evalFunctionWithName:SLUIARectEqualToRectFunctionName()
+                                                           withArgs:(@[ rect1, rect1 ])] boolValue],
+                 @"Two identical rects should be equal.");
+    SLAssertTrue([[[SLTerminal sharedTerminal] evalFunctionWithName:SLUIARectEqualToRectFunctionName()
+                                                           withArgs:(@[ @"null", @"null" ])] boolValue],
+                 @"Two null rects should be equal.");
+    SLAssertFalse([[[SLTerminal sharedTerminal] evalFunctionWithName:SLUIARectEqualToRectFunctionName()
+                                                            withArgs:(@[ rect1, rect2 ])] boolValue],
+                  @"Two different rects should not be equal.");
+}
+
+- (void)testSLUIARectContainsRect {
+    NSString *const containerRect =         @"{ origin: { x: 5.0, y: 10.0 }, size: { width: 50.0, height: 100.0 } }";
+    NSString *const containedWithinRect =   @"{ origin: { x: 10.0, y: 15.0 }, size: { width: 30.0, height: 50.0 } }";
+    NSString *const intersectingRect =      @"{ origin: { x: 10.0, y: 15.0 }, size: { width: 50.0, height: 100.0 } }";
+    NSString *const nonIntersectingRect =   @"{ origin: { x: 65.0, y: 120.0 }, size: { width: 50.0, height: 100.0 } }";
+
+    SLAssertTrue([[[SLTerminal sharedTerminal] evalFunctionWithName:SLUIARectContainsRectFunctionName()
+                                                           withArgs:(@[ containerRect, containerRect ])] boolValue],
+                 @"A rect should contain itself.");
+    SLAssertTrue([[[SLTerminal sharedTerminal] evalFunctionWithName:SLUIARectContainsRectFunctionName()
+                                                           withArgs:(@[ containerRect, containedWithinRect ])] boolValue],
+                 @"A rect should contain a rect within itself.");
+    SLAssertFalse([[[SLTerminal sharedTerminal] evalFunctionWithName:SLUIARectContainsRectFunctionName()
+                                                            withArgs:(@[ containerRect, intersectingRect])] boolValue],
+                  @"A rect should not contain a partially-intersecting rect.");
+    SLAssertFalse([[[SLTerminal sharedTerminal] evalFunctionWithName:SLUIARectContainsRectFunctionName()
+                                                            withArgs:(@[ containerRect, nonIntersectingRect])] boolValue],
+                  @"A rect should not contain a non-intersecting rect.");
 }
 
 @end
--- Sources/Classes/UIAutomation/SLGeometry.m
+++ Sources/Classes/UIAutomation/SLGeometry.m
@@ -10,8 +10,7 @@
 #import "SLTerminal+ConvenienceFunctions.h"
 
 
-NSString *SLUIARectFromCGRect(CGRect rect)
-{
+NSString *SLUIARectFromCGRect(CGRect rect) {
     NSCParameterAssert(!CGRectIsNull(rect));
     return [NSString stringWithFormat:@"{origin:{x:%f,y:%f}, size:{width:%f, height:%f}}",rect.origin.x,rect.origin.y,rect.size.width,rect.size.height];
 }
@@ -25,4 +24,33 @@ CGRect SLCGRectFromUIARect(NSString *UIARect) {
                                                                                  {' + rect.size.width + ',' + rect.size.height + '}}';"
                                                                       withArgs:@[ UIARect ]];
     return ([CGRectString length] ? CGRectFromString(CGRectString) : CGRectNull);
+}
+
+NSString *SLUIARectEqualToRectFunctionName() {
+    static NSString *const SLUIARectEqualToRectFunctionName = @"SLUIARectEqualToRect";
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [[SLTerminal sharedTerminal] loadFunctionWithName:SLUIARectEqualToRectFunctionName
+                                                   params:@[ @"rect1", @"rect2" ]
+                                                     body:@"return ((!rect1 && !rect2) ||\
+                                                                    ((rect2.origin.x === rect1.origin.x) &&\
+                                                                     (rect2.origin.y === rect1.origin.y) &&\
+                                                                     (rect2.size.width === rect1.size.width) &&\
+                                                                     (rect2.size.height === rect1.size.height)));"];
+    });
+    return SLUIARectEqualToRectFunctionName;
+}
+
+NSString *SLUIARectContainsRectFunctionName() {
+    static NSString *const SLUIARectContainsRectFunctionName = @"SLUIARectContainsRect";
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [[SLTerminal sharedTerminal] loadFunctionWithName:SLUIARectContainsRectFunctionName
+                                                   params:@[ @"rect1", @"rect2" ]
+                                                     body:@"return  ((rect2.origin.x >= rect1.origin.x) &&\
+                                                                     (rect2.origin.y >= rect1.origin.y) &&\
+                                                                     ((rect2.origin.x + rect2.size.width) <= (rect1.origin.x + rect1.size.width)) &&\
+                                                                     ((rect2.origin.y + rect2.size.height) <= (rect1.origin.y + rect1.size.height)));"];
+    });
+    return SLUIARectContainsRectFunctionName;
 }
