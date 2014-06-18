b5e3544632b1c6e10d42e95b6a2c7adc696bd90d
Jeff Wear <jeff@inkling.com>
2014-06-17 21:09:30 -0700

    Make `SLPickerViewTest` 64-bit compatible.

--- Integration Tests/Tests/SLPickerViewTest.m
+++ Integration Tests/Tests/SLPickerViewTest.m
@@ -42,7 +42,7 @@
 
 - (void)testPickerViewInitialComponentValues {
     NSArray *values = [UIAElement(_pickerView) valueOfPickerComponents];
-    SLAssertTrue([values count] == 2, @"SLPickerView should have 2 spinner components. Actual: %d", [values count]);
+    SLAssertTrue([values count] == 2, @"SLPickerView should have 2 spinner components. Actual: %lu", (unsigned long)[values count]);
     SLAssertTrue([[values objectAtIndex:0] isEqualToString:@"1. 1 of 3"], @"SLPickerView first spinner should have value '1. 1 of 3'. Actual: %@", [values objectAtIndex:0]);
     SLAssertTrue([[values objectAtIndex:1] isEqualToString:@"1. 1 of 3"], @"SLPickerView second spinner should have value '1. 1 of 3'. Actual: %@", [values objectAtIndex:1]);
 }
@@ -51,14 +51,14 @@
     // Select value 2 on the first picker component
     [UIAElement(_pickerView) selectValue:@"2" forComponent:0];
     NSArray *values = [UIAElement(_pickerView) valueOfPickerComponents];
-    SLAssertTrue([values count] == 2, @"SLPickerView should have 2 spinner components. Actual: %d", [values count]);
+    SLAssertTrue([values count] == 2, @"SLPickerView should have 2 spinner components. Actual: %lu", (unsigned long)[values count]);
     SLAssertTrue([[values objectAtIndex:0] isEqualToString:@"2. 2 of 3"], @"SLPickerView first spinner should have value '2. 1 of 3'. Actual: %@", [values objectAtIndex:0]);
     SLAssertTrue([[values objectAtIndex:1] isEqualToString:@"1. 1 of 3"], @"SLPickerView second spinner should have value '1. 1 of 3'. Actual: %@", [values objectAtIndex:1]);
 
     // Select value 3 on the second picker component
     [UIAElement(_pickerView) selectValue:@"3" forComponent:1];
     values = [UIAElement(_pickerView) valueOfPickerComponents];
-    SLAssertTrue([values count] == 2, @"SLPickerView should have 2 spinner components. Actual: %d", [values count]);
+    SLAssertTrue([values count] == 2, @"SLPickerView should have 2 spinner components. Actual: %lu", (unsigned long)[values count]);
     SLAssertTrue([[values objectAtIndex:0] isEqualToString:@"2. 2 of 3"], @"SLPickerView first spinner should have value '2. 1 of 3'. Actual: %@", [values objectAtIndex:0]);
     SLAssertTrue([[values objectAtIndex:1] isEqualToString:@"3. 3 of 3"], @"SLPickerView second spinner should have value '3. 3 of 3'. Actual: %@", [values objectAtIndex:1]);
 }
@@ -73,7 +73,7 @@
 
     // Values should be unchanged after failed changes
     NSArray *values = [UIAElement(_pickerView) valueOfPickerComponents];
-    SLAssertTrue([values count] == 2, @"SLPickerView should have 2 spinner components. Actual: %d", [values count]);
+    SLAssertTrue([values count] == 2, @"SLPickerView should have 2 spinner components. Actual: %lu", (unsigned long)[values count]);
     SLAssertTrue([[values objectAtIndex:0] isEqualToString:@"3. 3 of 3"], @"SLPickerView first spinner should have value '3. 1 of 3'. Actual: %@", [values objectAtIndex:0]);
     SLAssertTrue([[values objectAtIndex:1] isEqualToString:@"1. 1 of 3"], @"SLPickerView second spinner should have value '1. 1 of 3'. Actual: %@", [values objectAtIndex:1]);
 }
